f6b8fb3c955fefe0e5e6c6943a540b4c9fc7bc05
Israel Silva <escorpion45678@gmail.com>
2020-10-14 11:07:05 +0200

    Commit 1
    Trabajo realizado 13/10/2020

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Cliente, Categoria, Articulo, Reserva
+
+# admin.site.register(Cliente)
+admin.site.register(Categoria)
+# admin.site.register(Articulo)
+# admin.site.register(Reserva)
+
+class ClienteAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'apellidos', 'telefono')
+	fields = ['nombre', 'apellidos', ('telefono')]
+
+admin.site.register(Cliente, ClienteAdmin)
+
+@admin.register(Articulo)
+class ArticuloAdmin(admin.ModelAdmin):
+	list_display = ('nombre', 'precio')
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+	list_display = ('id', 'cliente', 'articulo', 'cantidad', 'fecha_reserva')
+	list_filter = ('estado', 'fecha_reserva')
+
+	fieldsets = (
+
+		('Compra', {
+			'fields': ('articulo', 'cantidad')
+		}),
+		('Disponibilidad', {
+			'fields': ('estado', 'fecha_reserva')
+		}),
+
+	)
--- catalogo/models.py
+++ catalogo/models.py
@@ -10,8 +10,8 @@ class Categoria(models.Model):
 
 class Articulo(models.Model):
 	nombre = models.CharField(max_length=100)
-	precio = models.IntegerField(default=0)
-	tipo = models.ForeingKey(Categoria, on_delete=models.SET_NULL, null=True, help_text="Selecciona una categoria.")
+	precio = models.FloatField(default=0)
+	tipo = models.ManyToManyField(Categoria, help_text="Selecciona una categoria.")
 
 	def __str__(self):
 		return self.nombre
@@ -19,14 +19,34 @@ class Articulo(models.Model):
 	def get_absolute_url(self):
 		return reverse('articulo-detail', args=[str(self.id)])
 
+class Cliente(models.Model):
+	nombre = models.CharField(max_length=30, help_text="Ingresa tu nombre.")
+	apellidos = models.CharField(max_length=50, help_text="Ingresa tus apellidos.")
+	telefono = models.IntegerField(help_text="Ingresa tu número de teléfono.")
+
+	def __str__(self):
+		return '%s %s, %s' % (self.nombre, self.apellidos, self.telefono)
+
+import uuid
+
 class Reserva(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID único para esta reserva")
-	fecha = models.DateField(null=True, blank=True)
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID único para esta reserva.")
+	fecha_reserva = models.DateField(null=True, blank=True)
+	cantidad = models.IntegerField(default=0, help_text="Ingresa la cantidad que quieres.")
+	articulo = models.ForeignKey(Articulo, on_delete=models.SET_NULL, null=True)
+	cliente = models.ForeignKey('Cliente', on_delete=models.SET_NULL, null=True)
 
 	LOAN_STATUS = (
 		('e', 'Entregado'),
 		('p', 'Pendiente'),
 		('c', 'Cancelada'),
+		('n', 'No disponible')
 	)
 
-	estado = 
+	estado = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='p', help_text='Estado del pedido.')
+
+	class Meta:
+		ordering = ["fecha_reserva"]
+
+	def __str__(self):
+		return '%s (%s, %s %s, %s)' % (self.id, self.articulo.nombre, self.cliente.nombre, self.cliente.apellidos, self.fecha_reserva)
